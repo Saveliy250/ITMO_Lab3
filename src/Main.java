83a6e4675c6f7501603b765c87b1ff6940fffd62
Saveliy250 <savepo@icloud.com>
2024-01-30 16:24:19 +0300

    idea first commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import Objects.Thing;
 public class Main {
     public static void main(String[] args) {
         Entity e1 = new Entity("Льюис");
-        Thing t1 = new Thing("лужа");
+        Thing t1 = new Thing("ла");
         e1.doSmth(Action.Fall, t1);
         }
     }
